a41c39dc65345e03b750d461b8085e9e285e3b22
Saivenkat1903 <86838056+Saivenkat1903@users.noreply.github.com>
2023-01-09 13:10:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+"""A basic Flask application to show the general framework."""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Congratulations, it's a web app!"
+    """Define main route into the website."""
+    return render_template('index.html')
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+if __name__ == '__main__':
+    app.run()
+    
